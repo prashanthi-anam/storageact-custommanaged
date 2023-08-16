98ae2a9762215b1eb394f6c2523a384567336715
prashanthi-anam <113832925+prashanthi-anam@users.noreply.github.com>
2023-08-16 15:08:16 +0530

    privateendpt.tf

--- vm-tomcat/privateendpt.tf
+++ vm-tomcat/privateendpt.tf
@@ -13,3 +13,20 @@ resource "azurerm_private_endpoint" "storage_endpoint" {
     is_manual_connection_assignment = false
   }
 }
+private_dns_zone_group {
+    name                 = "example-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+  }
+
+
+resource "azurerm_private_dns_zone" "example" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "example-link"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
