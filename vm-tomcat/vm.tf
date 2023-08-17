7b0b70aa45f7c3fe6a8be6e2926ae0371d34dc9c
prashanthi-anam <113832925+prashanthi-anam@users.noreply.github.com>
2023-08-17 13:13:48 +0530

    vm.tf

--- vm-tomcat/vm.tf
+++ vm-tomcat/vm.tf
@@ -25,6 +25,7 @@ resource "azurerm_subnet" "storage" {
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.3.0/24"]
+  enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_network_interface" "example" {
